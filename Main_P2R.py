85c3a6475a8c72919ce0633908bd5145ff832e0a
Kristos Akalugwu <kristosakalugwu@gmail.com>
2025-02-07 12:20:11 +0000

    Update Main_P2R.py

--- Main_P2R.py
+++ Main_P2R.py
@@ -80,7 +80,18 @@ def get_contingency_message(value):
 
 
 # Define a function to apply the color styling
-def colored_card(title, value, message, color):
+def colored_card(title, value, color,Percentage_full,Percentage):
+    st.markdown(
+        f"""
+        <div style="background-color:{color}; padding:10px; border-radius:10px; text-align:center; color:white; font-size:18px;">
+            <strong>{title}</strong><br>
+            <span style="font-size:24px;">{value:,.0f}</span><br>
+            <strong>{Percentage_full}</strong><br>
+            <span style="font-size:24px;">{Percentage:,.0f}%</span>
+            
+        </div>
+        """,
+        unsafe_allow_html=True
     )
 # Authentication for Google Sheets using Streamlit Secrets
 def authenticate_google_sheets():
